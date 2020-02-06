1790bf13cd72677c585c33b76c07c9ee2bad5fc6
Jonathan Ballard <asgardschosen@gmail.com>
2020-02-05 17:42:28 -0800

    migrated to ORM

--- app.py
+++ app.py
@@ -2,16 +2,21 @@
 # TODO
 # 
 # CREATE DB Classes --DONE-- 
-# Create DB relationships
-# Update all routes to use correct queries
-# Check templates for same
-# 
+# Create DB relationships --DONE--
+# Update all routes to use correct queries --DONE--
+# Check templates for same --DONE--
+# CURRENTLY SHOWS ALL TWEETS
 
 
 from flask import Flask, render_template, request, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy			# instead of mysqlconnection
 from sqlalchemy.sql import func                         # ADDED THIS LINE FOR DEFAULT TIMESTAMP
 from flask_migrate import Migrate			# this is new
+import re
+from flask_bcrypt import Bcrypt
+from sqlalchemy import text
+
+
 app = Flask(__name__)
 # configurations to tell our app about the database we'll be connecting to
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///$dojo_tweets_orm.db'
@@ -26,6 +31,16 @@ bcrypt = Bcrypt(app)     # we are creating an object called bcrypt,
 
 
 
+likes_table = db.Table('likes',
+db.Column('user_id', db.Integer, db.ForeignKey('Users.id', ondelete='cascade'), primary_key=True),
+db.Column('tweet_id', db.Integer, db.ForeignKey('Tweets.id', ondelete='cascade'), primary_key=True))
+
+
+follows_table = db.Table('follows',
+db.Column('user_being_followed', db.Integer, db.ForeignKey('Users.id', ondelete='cascade')))
+
+
+
 class Users(db.Model):	
     __tablename__ = "Users"    # optional		
     id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +48,14 @@ class Users(db.Model):
     last_name = db.Column(db.String(45))
     email = db.Column(db.String(45))
     password = db.Column(db.String(255))
+    tweet_id = db.relationship('Tweets', secondary=likes_table)
+    user_being_followed = db.relationship('Users', secondary=follows_table)
+    user_following = db.relationship('Users', secondary=follows_table)
+
+    # group_id = db.Column(db.ForeignKey(Group.id))
+    # group = db.relationship(Group, backref='users')
+
+
     created_at = db.Column(db.DateTime, server_default=func.now())    # notice the extra import statement above
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
 
@@ -42,6 +65,7 @@ class Tweets(db.Model):
     __tablename__ = "Tweets"    # optional		
     id = db.Column(db.Integer, primary_key=True)
     tweet = db.Column(db.String(255))
+    user_id = db.relationship('Users', secondary=likes_table)
     created_at = db.Column(db.DateTime, server_default=func.now())    # notice the extra import statement above
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
 
@@ -55,16 +79,15 @@ class Tweets(db.Model):
 
 @app.route('/')
 def index():
-    mysql = connectToMySQL("dojo_tweets")
-    users = mysql.query_db("SELECT * FROM users;")
+    users = Users.query.all()
     print(users)
     return render_template("index.html", all_users = users)
 
 
 @app.route('/register', methods=['POST'])
 def register():
-    mysql = connectToMySQL("dojo_tweets")
-    users = mysql.query_db("SELECT * FROM users;")
+
+    users = Users.query.all()
     print(users)
     EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
     PW_REGEX = re.compile(r'^.*(?=.{8,10})(?=.*[a-zA-Z])(?=.*?[A-Z])(?=.*\d)[a-zA-Z0-9!@£$%^&*()_+={}?:~\[\]]+$')
@@ -117,18 +140,11 @@ def register():
     
     
     if isValid == True:
-        mysql = connectToMySQL("dojo_tweets")
-        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(fname)s, %(lname)s, %(email)s, %(pw)s);"
-        data = {
-            "fname": firstName,
-            "lname": lastName,
-            "email": email,
-            "pw": pwHash
-        }
-        new_user_id = mysql.query_db(query, data)
+        new_user = Users(first_name = firstName, last_name =lastName, email = email, password=pwHash)
+        db.session.add(new_user)
+        db.session.commit()
 
-        mysql = connectToMySQL("dojo_tweets")
-        users = mysql.query_db("SELECT * FROM users;")
+        users = Users.query.all()
         print(users)
         flash('Success!')
         return redirect('/')
@@ -142,55 +158,50 @@ def destroy():
 
 @app.route('/login', methods=['POST'])
 def login():
-    mysql = connectToMySQL("dojo_tweets")
-    emailDB = mysql.query_db("SELECT email FROM users;")
-    print(emailDB)
+
+
     form = request.form['formType']
 
     email = request.form['emailLogin']
     password = str(request.form['passwordLogin'])
+
     data = {
         "em": email
     }
 
-    # check emails for request.form['email']
-    mysql = connectToMySQL("dojo_tweets")
-    query = "SELECT email FROM users WHERE email = %(em)s;"
-    emailCheckDB = mysql.query_db(query, data)
 
+    # query = "SELECT email FROM users WHERE email = %(em)s;"
+    emailCheckDB = Users.query.filter_by(email = email).all()
+    print('TEEEEEEEEEEEEEEEEEEEEEST', emailCheckDB)
     # check if emailCheckDB is populated
     if len(emailCheckDB) == 0:
         emailCheckDBFull = False
     else:
         emailCheckDBFull = True
 
-    mysql = connectToMySQL("dojo_tweets")
-    query = "SELECT password FROM users WHERE email= %(em)s;"
-    login_id = mysql.query_db(query, data)
+    login_id = Users.query.filter_by(email = email).all()
+    # query = "SELECT password FROM users WHERE email= %(em)s;"
     
     
-    # userId
-    mysql = connectToMySQL("dojo_tweets")
-    query = "SELECT first_name FROM users WHERE email = %(em)s;"
-    userId = mysql.query_db(query, data)
-
+    # user first name
+    # query = "SELECT first_name FROM users WHERE email = %(em)s;"
+    userId = Users.query.filter_by(email = email).all()
 
     # userId
-    mysql = connectToMySQL("dojo_tweets")
-    query = "SELECT id FROM users WHERE email = %(em)s;"
-    idDict = mysql.query_db(query, data)
 
+    # query = "SELECT id FROM users WHERE email = %(em)s;"
+    idDict = Users.query.filter_by(email = email).all()
+
+    print('idDict^%^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^', idDict)
     if len(idDict) > 0:
-        session['id'] = idDict[0]['id']
+        session['id'] = idDict[0].id
         
-    # print('login_id ***********************************', login_id[0]['password'])
-    # if not str(email) in emailDB:
     if not emailCheckDBFull:
         print('EmailCheck ****************************', 'email')
         flash('Email not in our database', 'login')
         return redirect('/')
     else:
-        hashCheck = bcrypt.check_password_hash(login_id[0]['password'], password)
+        hashCheck = bcrypt.check_password_hash(login_id[0].password, password)
         print('hashCheck ***************************************', hashCheck)
         if not hashCheck:
             flash('Invalid Password', 'login')
@@ -198,7 +209,7 @@ def login():
         else:
             flash("Successfully Logged In!")
             if not 'user_id' in session:
-                session['user_id'] = userId[0]['first_name']
+                session['user_id'] = userId[0].first_name
                 print('Session[user_id] *******---------------+***********', session['user_id'])
             return redirect('/dashboard')
 
@@ -209,22 +220,21 @@ def dashboard():
     if not session['id']:
         return redirect('/')
 
-    mysql = connectToMySQL("dojo_tweets")
-    userFriends = mysql.query_db(f"SELECT followed_user_id FROM follows WHERE follower_user_id = {session['id']} OR user_id = {session['id']}")
-
+    
+    # userFriends = mysql.query_db(f"SELECT followed_user_id FROM follows WHERE follower_user_id = {session['id']} OR user_id = {session['id']}")
+    # userFriends = Users.query('user_being_followed').filter('user_following' == session['id'] OR 'id' == session['id'])
 
     # return list of tweets for welcome.html
-    mysql = connectToMySQL("dojo_tweets")
     # tweetList = mysql.query_db(f"SELECT * FROM tweets")   #SELECT ALL TWEETS
     # tweetList = mysql.query_db(f"SELECT * FROM tweets WHERE user_id = {session['id']}")  #ONLY SHOW TWEETS THAT PERSON MADE
 
 
     friendsList = list()
 
-    for index in range(len(userFriends)):
-        print('INDEX+++++++++++++++++++++++++++++++++', index)
+    # for index in range(len(userFriends)):
+        # print('INDEX+++++++++++++++++++++++++++++++++', index)
         # friendsList[index].append(userFriends[index]['followed_user_id'])
-        x = userFriends[index]['followed_user_id']
+        # x = userFriends[index]['followed_user_id']
 
     # friendsList = [4,6]
 
@@ -233,8 +243,11 @@ def dashboard():
 
 
     
-    tweetList = mysql.query_db(f"SELECT * FROM tweets JOIN users ON tweets.user_id = users.id JOIN follows ON users.id = follows.user_id")  #SELECT MY AND FRIENDS TWEETS
+    # tweetList = mysql.query_db(f"SELECT * FROM tweets JOIN users ON tweets.user_id = users.id JOIN follows ON users.id = follows.user_id")  #SELECT MY AND FRIENDS TWEETS
+    tweetList = Tweets.query.all()
+    usersList = Users.query.all()
     print('TWEETLIST =======__________________________________=====', tweetList)
+
 
     tweetTuple = ()
     friendsList.append(session['id'])
@@ -242,25 +255,46 @@ def dashboard():
 
 
     for index in range(len(tweetList)):
-        if tweetList[index]['follower_user_id'] == session['id'] or tweetList[index]['user_id'] == session['id']:
-            friendsList.append(tweetList[index]['followed_user_id'])
+        if tweetList[index].user_id == session['id']:
+            friendsList.append(tweetList[index].user_id)
+
+    for index in range(len(usersList)):
+        if usersList[index].user_following == session['id']:
+            friendsList.append(usersList[index].user_being_followed)
+
+            
 
 
-    mysql = connectToMySQL("dojo_tweets")
     if len(friendsList):
         tweetTuple = tuple(friendsList)
     else:
         tweetTuple = tuple()
 
-    tweetList2 = mysql.query_db(f"SELECT * FROM tweets WHERE user_id IN {tweetTuple}")
+    tweetList3 = list(tweetTuple)
 
+    # tweetList2 = Tweets.query.from_statement(text(f"SELECT * FROM Tweets WHERE users_who_like_this_tweet IN {tweetTuple}"))
+    # tweetList2 = Tweets.query.filter_by(Users.user_being_followed.in_(tweetList3))
+    # tweetList2 = mysql.query_db(f"SELECT * FROM tweets WHERE user_id IN {tweetTuple}")
+    tweetList2 = Tweets.query.all()
+    tweetListFinal = list()
+
+    for tweet in range(len(tweetList2)):
+        print('tweet.user_id   323222222222222222222222222222222222222222222222', tweetList2[tweet].user_id)
+        if tweetList2[tweet].user_id in tweetList3:
+            print('++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ appending')
+            tweetListFinal.append(tweetList2[tweet])
+    
     # tweetList = mysql.query_db(f"SELECT tweets.id, tweets.tweet, tweets.created_at, tweets.updated_at, tweets.user_id, follows.followed_user_id, follows.follower_user_id FROM tweets JOIN users ON tweets.user_id = users.id JOIN follows ON users.id = follows.user_id WHERE tweets.user_id IN {followed_users} GROUP BY tweets.id")
-
+    
+    print('tweetList2   23222222222222222222222222222222222222222222222', tweetList2)
+    print('tweetList3   23222222222222222222222222222222222222222222222', tweetList3)
+    print('tweetListFinal   23222222222222222222222222222222222222222222222', tweetListFinal)
 
 
     # get number of likes to display per tweet
-    mysql = connectToMySQL("dojo_tweets")
-    likesNum = mysql.query_db("SELECT COUNT(id) FROM likes GROUP BY tweet_id")
+
+    # likesNum = mysql.query_db("SELECT COUNT(id) FROM likes GROUP BY tweet_id")
+    likesNum = 55
     print('LIKESNUM=======55555555555555555555555555=====', likesNum)
 
     return render_template('welcome.html', tweetList = tweetList2, likesNum = likesNum)
@@ -285,15 +319,15 @@ def tweet_create():
 
     if isValid:
         # enter tweet into database
-        mysql = connectToMySQL("dojo_tweets")
-        data = {
-            'id': session['id'],
-            'tweet': incomingTweet
-        }
+        
+        
         print('SESSION ID----------*******************************************', session['id'])
         print('TWEET----------*******************************************', incomingTweet)
-        query = "INSERT INTO tweets (tweet, user_id) VALUES(%(tweet)s, %(id)s);"
-        tweet_add = mysql.query_db(query, data)
+
+        new_tweet = Tweets(tweet = incomingTweet)
+        db.session.add(new_tweet)
+        db.session.commit()
+        # query = "INSERT INTO tweets (tweet, user_id) VALUES(%(tweet)s, %(id)s);"
 
 
     return redirect('/dashboard')
@@ -307,15 +341,17 @@ def delete_tweet(id):
     print('DELETE ID----------*******************************************', id)
     
 
-    mysql = connectToMySQL("dojo_tweets")
-    tweet_check = mysql.query_db(f"SELECT user_id FROM tweets WHERE id={id};")
+    tweet_check = Tweets.query.filter_by(id = id).all()
+    # tweet_check = mysql.query_db(f"SELECT user_id FROM tweets WHERE id={id};")
 
     print('TWEET CHECK ID----------*******************************************', tweet_check)
 
     # if userId of tweet = session['id]
     if tweet_check[0]['user_id'] == session['id']:
-        mysql = connectToMySQL("dojo_tweets")
-        tweet_delete = mysql.query_db(f"DELETE FROM tweets WHERE id={id};")
+        tweet_delete = Tweets.query.get(id)
+        db.session.delete(tweet_delete)
+        db.session.commit()
+        # tweet_delete = mysql.query_db(f"DELETE FROM tweets WHERE id={id};")
     else:
         flash('invalid user')
 
@@ -334,17 +370,19 @@ def update_tweet(id):
 
     t_id = int(id)
     tweet = request.form['tweet']
-    mysql = connectToMySQL("dojo_tweets")
-    data = {
-        'tweet': tweet,
-        'id': t_id
-    }
-    query = "UPDATE tweets SET tweet = %(tweet)s WHERE id=%(id)s;"
-    print('leng request.form tweet55555555555555555555555555555', len(request.form['tweet']))
+    
+    
+
+    # query = "UPDATE tweets SET tweet = %(tweet)s WHERE id=%(id)s;"
+    
+
     if len(request.form['tweet']) < 1 or len(request.form['tweet']) > 255:
         flash('Invalid tweet length')
     else:    
-        tweet_edit = mysql.query_db(query, data)
+        tweet_edit = Tweets.query.filter_by(id = t_id)
+        # tweet_edit = mysql.query_db(query, data)
+        tweet_edit.tweet = tweet
+        db.session.commit()
     return redirect('/dashboard')
     
 
@@ -360,8 +398,10 @@ def like_tweet(id):
 
     # add like to DB
     userId = session['id']
-    mysql = connectToMySQL("dojo_tweets")
-    tweet_like = mysql.query_db(f"INSERT INTO likes (user_id, tweet_id) VALUES({userId}, {id});")
+    tweet_to_like = Tweets.query.get(id)
+    user = Users.query.get(session['id'])
+    tweet_to_like.user_id.append(user)
+    # tweet_like = mysql.query_db(f"INSERT INTO likes (user_id, tweet_id) VALUES({userId}, {id});")
     print('USER ID----------*******************************************', userId)
     print('TWEET ID----------*******************************************', id)
     return redirect('/dashboard')
@@ -372,8 +412,8 @@ def like_tweet(id):
 @app.route('/users')
 def get_users():
 
-    mysql = connectToMySQL("dojo_tweets")
-    users = mysql.query_db("SELECT * FROM users;")
+   
+    users = Users.query.all()
 
     return render_template('/users.html',  users = users)
 
@@ -382,9 +422,19 @@ def get_users():
 @app.route('/users/<id>/follow')
 def follow_user(id):
 
-    mysql = connectToMySQL("dojo_tweets")
+    
     intId = int(id)
-    follow = mysql.query_db(f"INSERT INTO follows (followed_user_id, follower_user_id, user_id) VALUES ({intId}, {session['id']}, {session['id']} );")
+
+    userId = session['id']
+    user_to_follow = Users.query.get(id)
+    user = Users.query.get(session['id'])
+    user.user_being_followed.append(user_to_follow)
+
+
+
+    # follow = mysql.query_db(f"INSERT INTO follows (followed_user_id, follower_user_id, user_id) VALUES ({intId}, {session['id']}, {session['id']} );")
+
+
     print('FOLLOW**********************************************************', )
 
     return redirect('/dashboard')
